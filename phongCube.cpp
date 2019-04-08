4206aeda07002578553b5a8f14c6a3345801d77f
Jeremy Ryan Chandler <jeremy@macbook.local>
2019-04-08 11:37:19 -0600

    first commit

---
+++ phongCube.cpp
@@ -0,0 +1,241 @@
+#define SDL_MAIN_HANDLED
+#include "/usr/local/include/SDL2/SDL.h"
+#include "gl.h"
+
+// compile command
+// g++ gl.cpp -lopengl -lglew -lSDL2
+
+int main(int aargc, char **argv) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* win = SDL_CreateWindow("OpenGL Program", 200, 200,
+        SCREEN_W, SCREEN_H, SDL_WINDOW_OPENGL);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetSwapInterval(1);
+    SDL_GLContext cont = SDL_GL_CreateContext(win);
+
+    glewInit();
+    glEnable(GL_DEPTH_TEST);
+
+    bool success = true;
+    string vertexShader;
+    string secondVertexShader;
+    string fragmentShader;
+
+    int vertexHandle;
+    int secondVertexHandle;
+    int fragHandle;
+    int programHandle;
+    int secondProgramHandle;
+
+    success &= parseFile((char*) "vertexBillboard.vs", vertexShader); //vertexBillboard //phongVertex2
+    success &= parseFile((char*) "phongFrag2.fs", fragmentShader);
+    success &= compileShader(vertexShader.c_str(), GL_VERTEX_SHADER, vertexHandle);
+    success &= compileShader(fragmentShader.c_str(), GL_FRAGMENT_SHADER, fragHandle);
+    success &= compileProgram(vertexHandle, fragHandle, programHandle);
+
+    success &= parseFile((char*) "phongVertex2.vs", secondVertexShader); //vertexBillboard //phongVertex2
+    success &= compileShader(secondVertexShader.c_str(), GL_VERTEX_SHADER, secondVertexHandle);
+    success &= compileProgram(secondVertexHandle, fragHandle, secondProgramHandle);
+
+    /********************************
+    * Object Loader
+    * ******************************/
+    vector<material> materials;
+    vector<vertexData> vertexBuffer;
+
+    bool hasUV;
+    bool hasNormal;
+
+    success &= getObjData("bunny.obj", materials, vertexBuffer, hasUV, hasNormal);
+
+    // Build out a single array of floats
+    int stride = 3 + 2 * hasUV + 3 * hasNormal;
+    int vertexBufferNumBytes = stride * vertexBuffer.size() * sizeof(float);
+    float *vertexBufferData = (float *)(malloc(vertexBufferNumBytes));
+
+    int i = 0;
+    // Join data into interleaved buffer;
+    for (int vb = 0; vb < vertexBuffer.size(); vb++) {
+        vertexBufferData[i++] = vertexBuffer[vb].vert[0];
+        vertexBufferData[i++] = vertexBuffer[vb].vert[1];
+        vertexBufferData[i++] = vertexBuffer[vb].vert[2];
+
+        if (hasUV) {
+            vertexBufferData[i++] = vertexBuffer[vb].uv[0];
+            vertexBufferData[i++] = vertexBuffer[vb].uv[1];
+        }
+
+        if (hasNormal) {
+            vertexBufferData[i++] = vertexBuffer[vb].normal[0];
+            vertexBufferData[i++] = vertexBuffer[vb].normal[1];
+            vertexBufferData[i++] = vertexBuffer[vb].normal[2];
+        }
+
+    }
+
+    vector<int> textureIDs;
+    for (int mat = 0; mat < materials.size(); mat++) {
+        int tmp;
+        material m = materials[mat];
+        success &= loadTexture(m.map_Kd, tmp);
+        textureIDs.push_back(tmp);
+    }
+
+    validate(success, (char*) "Setup OpenGL Program");
+
+
+    /********************************
+    * Vertex Buffer Object
+    * ******************************/
+   int VBO;
+   glGenBuffers(1, (GLuint *) &VBO);
+   glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+   glBufferData(GL_ARRAY_BUFFER, vertexBufferNumBytes, vertexBufferData, GL_STATIC_DRAW);
+
+    /********************************
+    * Attribute Handles
+    * ******************************/
+   int aPositionHandle = glGetAttribLocation(programHandle, "a_Position");
+   int aUVHandle = glGetAttribLocation(programHandle, "a_UV");
+   int aNormalHandle = glGetAttribLocation(programHandle, "a_Normal");
+
+   /********************************
+   * Uniform Handles
+   * ******************************/
+   int uMatrixHandle = glGetUniformLocation(programHandle, "u_Matrix");
+   int uModelHandle = glGetUniformLocation(programHandle, "u_Model");
+   int uViewHandle = glGetUniformLocation(programHandle, "u_View");
+   int uProjHandle = glGetUniformLocation(programHandle, "u_Projection");
+   int uTextureHandle = glGetUniformLocation(programHandle, "u_Texture");
+   int uAmbientStrengthHandle = glGetUniformLocation(programHandle, "ambientStrength");
+   int uDiffuseStrengthHandle = glGetUniformLocation(programHandle, "diffuseStrength");
+   int uSpecularStrengthHandle = glGetUniformLocation(programHandle, "specularStrength");
+   int uLightPosHandle = glGetUniformLocation(programHandle, "lightPos");
+   int uViewPosHandle = glGetUniformLocation(programHandle, "viewPos");
+   int uLightColorHandle = glGetUniformLocation(programHandle, "lightColor");
+   //int uThresholdHandle = glGetUniformLocation(programHandle, "u_Threshold");
+
+   /**********************************
+    * 
+    *  Second program
+    * ********************************/
+
+    /********************************
+    * Attribute Handles
+    * ******************************/
+   int aPositionHandle1 = glGetAttribLocation(secondProgramHandle, "a_Position");
+   int aUVHandle1 = glGetAttribLocation(secondProgramHandle, "a_UV");
+   int aNormalHandle1 = glGetAttribLocation(secondProgramHandle, "a_Normal");
+
+   /********************************
+   * Uniform Handles
+   * ******************************/
+   int uMatrixHandle1 = glGetUniformLocation(secondProgramHandle, "u_Matrix");
+   int uModelHandle1 = glGetUniformLocation(secondProgramHandle, "u_Model");
+   int uViewHandle1 = glGetUniformLocation(secondProgramHandle, "u_View");
+   int uProjHandle1 = glGetUniformLocation(secondProgramHandle, "u_Projection");
+   int uTextureHandle1 = glGetUniformLocation(secondProgramHandle, "u_Texture");
+   int uAmbientStrengthHandle1 = glGetUniformLocation(secondProgramHandle, "ambientStrength");
+   int uDiffuseStrengthHandle1 = glGetUniformLocation(secondProgramHandle, "diffuseStrength");
+   int uSpecularStrengthHandle1 = glGetUniformLocation(secondProgramHandle, "specularStrength");
+   int uLightPosHandle1 = glGetUniformLocation(secondProgramHandle, "lightPos");
+   int uViewPosHandle1 = glGetUniformLocation(secondProgramHandle, "viewPos");
+   int uLightColorHandle1 = glGetUniformLocation(secondProgramHandle, "lightColor");
+   //int uThresholdHandle1 = glGetUniformLocation(secondProgramHandle, "u_Threshold");
+
+   // MVP Matrix
+   mat4 mvp;
+   mat4 model;
+   mat4 view;
+   mat4 proj;
+   
+   // Setup Camera Data
+   myCam.camX = myCam.camY = myCam.camZ = myCam.pitch = myCam.yaw = myCam.roll = 0.0;
+
+   int numDraw = vertexBuffer.size();
+   bool running = true;
+   int frame = 0;
+
+   while(running) {
+       processUserInputs(running);
+
+       {
+           // Clear buffers
+           glClearColor(0, 0, 0, 1.0);
+           glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+           if(currentCycle == BILLBOARD)
+            glUseProgram(programHandle);
+           else
+            glUseProgram(secondProgramHandle);
+
+            frame++;
+
+           /*************************
+            * Setup Attributes
+            *************************/
+           glBindBuffer(GL_ARRAY_BUFFER, VBO);
+           glVertexAttribPointer(aPositionHandle, 3, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)0);
+           glEnableVertexAttribArray(aPositionHandle);
+
+           glVertexAttribPointer(aUVHandle, 2, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)(0 + 3*sizeof(float)));
+           glEnableVertexAttribArray(aUVHandle);
+
+           glVertexAttribPointer(aNormalHandle, 3, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)(0 + 5*sizeof(float)));
+           glEnableVertexAttribArray(aNormalHandle);
+           /*************************
+            * Setup Uniforms
+            *************************/
+           // Update texture
+           glActiveTexture(GL_TEXTURE0 + 0);
+           glBindTexture(GL_TEXTURE_2D, textureIDs[0]);
+
+           //glUniform1i(uTextureHandle, 0);
+           //glUniform1f(uThresholdHandle, threshold);
+
+           if(currentCycle == BILLBOARD)
+            {
+                glUseProgram(programHandle);
+                glUniform3f(uAmbientStrengthHandle, 0.2,0.2,0.2);
+                glUniform3f(uDiffuseStrengthHandle, 0.64,0.64,0.64);
+                glUniform4f(uSpecularStrengthHandle, 0.9,0.9,0.9,0.9);
+                glUniform3f(uLightPosHandle, 1.0,1.2,4.0);
+                glUniform3f(uViewPosHandle, 1.0,1.0,1.0);
+                glUniform4f(uLightColorHandle, 1.0,1.0,1.0,1.0);
+
+                setupMVP(mvp,model,view,proj);
+                glUniformMatrix4fv(uMatrixHandle, 1, false, &mvp[0][0]);
+                glUniformMatrix4fv(uModelHandle, 1, false, &model[0][0]);
+                glUniformMatrix4fv(uViewHandle, 1, false, &view[0][0]);
+                glUniformMatrix4fv(uProjHandle, 1, false, &proj[0][0]);
+            }
+           else
+           {
+                glUseProgram(secondProgramHandle);
+                glUniform3f(uAmbientStrengthHandle1, 0.2,0.2,0.2);
+                glUniform3f(uDiffuseStrengthHandle1, 0.64,0.64,0.64);
+                glUniform4f(uSpecularStrengthHandle1, 0.9,0.9,0.9,0.9);
+                glUniform3f(uLightPosHandle1, 1.0,1.2,4.0);
+                glUniform3f(uViewPosHandle1, 1.0,1.0,1.0);
+                glUniform4f(uLightColorHandle1, 1.0,1.0,1.0,1.0);
+
+                setupMVP(mvp,model,view,proj);
+                glUniformMatrix4fv(uMatrixHandle1, 1, false, &mvp[0][0]);
+                glUniformMatrix4fv(uModelHandle1, 1, false, &model[0][0]);
+                glUniformMatrix4fv(uViewHandle1, 1, false, &view[0][0]);
+                glUniformMatrix4fv(uProjHandle1, 1, false, &proj[0][0]);
+               
+           }
+
+           glDrawArrays(GL_TRIANGLES, 0, numDraw);
+       }
+
+       SDL_GL_SwapWindow(win);
+   }
+
+   free(vertexBufferData);
+   
+   return 0;
+}
---
+++ vertexBillboard.vs
@@ -0,0 +1,42 @@
+uniform mat4 u_Matrix;
+uniform mat4 u_Model;
+uniform mat4 u_View;
+uniform mat4 u_Projection;
+
+attribute vec3 a_Position;
+attribute vec2 a_UV;
+attribute vec3 a_Normal;
+
+varying vec2 v_UV;
+varying vec3 v_Normal;
+varying vec3 v_Position;
+
+void main()
+{
+   mat4 ModelView = u_View * u_Model;
+   // Column 0:
+   ModelView[0][0] = 40.0;
+   ModelView[0][1] = 0.0;
+   ModelView[0][2] = 0.0;
+
+   // Column 1:
+   //ModelView[1][0] = 0.0;
+   //ModelView[1][1] = 40.0;
+   //ModelView[1][2] = 0.0;
+
+   // Column 2:
+   ModelView[2][0] = 0.0;
+   ModelView[2][1] = 0.0;
+   ModelView[2][2] = 40.0;
+
+
+
+   v_UV = a_UV;
+   v_Normal = vec3(ModelView * vec4(a_Normal,0.0));
+   v_Position = vec3(ModelView * vec4(a_Position,1.0));
+
+   //gl_Position = ModelView * vec4(a_Position, 1.0);
+   //gl_Position /= gl_Position.w;
+   //gl_Position.xy += a_Position.xy * vec2(0.2,0.05);
+    gl_Position = u_Projection * ModelView * vec4(a_Position, 1.0);
+}
